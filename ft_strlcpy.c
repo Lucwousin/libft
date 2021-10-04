53e8af5078e7ea23a96052f067b9baedfb256ecb
Lucas Sinke <lsinke@student.codam.nl>
2021-10-04 15:22:17 +0200

    add ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+size_t	strlcpy(char *dst, const char *src, size_t dstlen)
+{
+	size_t	srclen;
+
+	srclen = ft_strlen(src);
+	if (srclen + 1 < dstlen)
+		ft_memcpy(dst, src, srclen + 1);
+	else if (dstlen != 0)
+	{
+		ft_memcpy(dst, src, dstlen - 1);
+		dst[dstlen - 1] = '\0';
+	}
+	return (srclen);
+}
--- libft.h
+++ libft.h
@@ -11,5 +11,6 @@ void	*ft_memset(void *dst, int c, size_t len);
 void	ft_bzero(void *dst, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstlen);
 
 #endif /* LIBFT_H */
