5f235379f0f0776a26a60c45c2efb726e979e2cc
Lucas Sinke <lsinke@student.codam.nl>
2021-10-04 13:32:33 +0200

    add ft_bzero

--- libft.h
+++ libft.h
@@ -8,5 +8,6 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *dest, int c, size_t len);
+void	ft_bzero(void *dest, size_t len);
 
 #endif /* LIBFT_H */
