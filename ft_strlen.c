b2fd313f6e9493bf0b583187c6ac832522d8b98a
Lucas Sinke <lsinke@student.codam.nl>
2021-10-04 13:13:00 +0200

    add ft_strlen

---
+++ ft_strlen.c
@@ -0,0 +1,9 @@
+size_t	ft_strlen(const char *s)
+{
+	size_t	l;
+
+	l = 0;
+	while (s[l] != '\0')
+		++l;
+	return (l);
+}
--- libft.h
+++ libft.h
@@ -1,10 +1,11 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *str);
 
 #endif /* LIBFT_H */
