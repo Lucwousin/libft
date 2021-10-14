4a1172f9c146beb9b51dc91fa4d9db3c0bbacb24
Lucas Sinke <lsinke@student.codam.nl>
2021-10-14 15:44:08 +0200

    Only protect strjoin for str1 being null

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,17 +1,5 @@
 #include "libft.h"
 
-static void	get_lens(const char *s1, const char *s2, size_t *l1, size_t *l2)
-{
-	if (s1)
-		*l1 = ft_strlen(s1);
-	else
-		*l1 = 0;
-	if (s2)
-		*l2 = ft_strlen(s2);
-	else
-		*l2 = 0;
-}
-
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*str;
@@ -19,7 +7,10 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	size_t	l2;
 	size_t	len;
 
-	get_lens(s1, s2, &l1, &l2);
+	if (!s1)
+		return (ft_strdup(s2));
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
 	if (l1 == 0 && l2 == 0)
 		return (ft_strdup(""));
 	if (l1 == 0)
