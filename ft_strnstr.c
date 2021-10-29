9b35ae31f6a2cb38d8cd707557020ae39a157272
Lucas Sinke <lsinke@student.codam.nl>
2021-10-29 13:36:12 +0200

    Simplify strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,27 +1,29 @@
 #include "libft.h"
 
+int	try_find(const char *str, const char *find, size_t n)
+{
+	while (n != 0 && *find)
+	{
+		if (*find != *str)
+			return (0);
+		++find;
+		++str;
+		--n;
+	}
+	return (*find == 0);
+}
+
 char	*ft_strnstr(const char *str, const char *find, size_t n)
 {
-	size_t	len_f;
-	size_t	off;
-	char	*found;
-
-	if (*str == '\0')
-		return (NULL);
-	len_f = ft_strlen(find);
-	if (len_f == 0 || str == find)
+	if (*find == '\0' || str == find)
 		return ((char *) str);
-	off = 0;
-	while (1)
+	while (n > 0 && *str)
 	{
-		found = ft_memchr(str + off, find[0], n - off);
-		if (!found)
-			return (NULL);
-		off = found - str + 1;
-		if (off > n || off + len_f > n)
-			return (NULL);
-		if (ft_strncmp(found, find, len_f) == 0)
-			return (found);
-		found = NULL;
+		if (*str == *find)
+			if (try_find(str, find, n))
+				return ((char *) str);
+		++str;
+		--n;
 	}
+	return (NULL);
 }
