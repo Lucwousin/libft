22e3c02903672facb45bae3a5773bb22153e01fa
Lucas Sinke <lsinke@student.codam.nl>
2022-08-25 15:22:34 +0200

    Add const to putstr functions

--- include/libft.h
+++ include/libft.h
@@ -67,8 +67,8 @@ void			*ft_realloc(void *oldptr, size_t oldsize, size_t newsize);
 
 // File write functions
 void			ft_putchar_fd(char c, int fd);
-void			ft_putstr_fd(char *s, int fd);
-void			ft_putendl_fd(char *s, int fd);
+void			ft_putstr_fd(const char *s, int fd);
+void			ft_putendl_fd(const char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
 
 // Simple math
