f250f536f3b635094b269364bccd3f1d2817c013
Lucas Sinke <lsinke@student.codam.nl>
2022-09-14 19:58:34 +0200

    Add ft_isspace function

--- include/libft.h
+++ include/libft.h
@@ -23,6 +23,7 @@ int				ft_isdigit(int c);
 int				ft_isalnum(int c);
 int				ft_isascii(int c);
 int				ft_isprint(int c);
+bool			ft_isspace(int c);
 
 // Character manipulation functions
 int				ft_toupper(int c);
